0a52d299045137f4ad1ed47eed16ac380d8b85fc
ahmed-yasser12 <ahmedyasser541@gmail.com>
2026-03-10 01:16:44 +0200

    solve problem in pagination

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -66,7 +66,7 @@ function Products() {
 
   const currentProducts = filteredProducts.slice(
     startIndex,
-    startIndex + itemsPerPage
+    startIndex + itemsPerPage,
   );
 
   if (loading) {
@@ -74,21 +74,21 @@ function Products() {
       // // <div className="flex flex-col items-center justify-center py-70">
       //   {/* <div className="w-12 h-12 border-4 border-gray-200 border-t-blue-600 rounded-full animate-spin mb-4" />
       //   <p className="text-gray-500 text-sm">Loading products...</p> */}
-         
+
       // {/* </div> */}
       <>
-      <div className="grid grid-cols-1 my-40 sm:grid-cols-2 md:grid-cols-3">
-       { Array.from({ length: 3}).map((_, i) => (
+        <div className="grid grid-cols-1 my-40 sm:grid-cols-2 md:grid-cols-3">
+          {Array.from({ length: 3 }).map((_, i) => (
             <ProductCardSkeleton key={i} />
           ))}
-      </div>
+        </div>
       </>
     );
   }
 
   return (
     <section className="max-w-7xl mx-auto px-4 py-16">
-        <Helmet>
+      <Helmet>
         <title>Products | ShopHub</title>
       </Helmet>
       <h3 className="text-3xl font-bold">Our Products</h3>
@@ -99,17 +99,13 @@ function Products() {
           type="text"
           placeholder="Search products..."
           value={search}
-          onChange={(e) =>
-            updateParams({ search: e.target.value, page: 1 })
-          }
+          onChange={(e) => updateParams({ search: e.target.value, page: 1 })}
           className="border w-[50%] border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
 
         <select
           value={selectedCategory}
-          onChange={(e) =>
-            updateParams({ category: e.target.value, page: 1 })
-          }
+          onChange={(e) => updateParams({ category: e.target.value, page: 1 })}
           className="border border-gray-300 rounded-lg py-2 px-4"
         >
           <option value="">All Categories</option>
@@ -124,9 +120,7 @@ function Products() {
 
       {/* Products */}
       {currentProducts.length === 0 ? (
-        <p className="text-center text-gray-500 py-20">
-          No products found.
-        </p>
+        <p className="text-center text-gray-500 py-20">No products found.</p>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 my-10">
           <ProductItems products={currentProducts} />
@@ -138,15 +132,16 @@ function Products() {
         <div className="flex justify-center items-center gap-2 mt-8">
           {/* Prev */}
           <button
-            onClick={() =>
-              updateParams({ page: Math.max(currentPage - 1, 1) })
-            }
-            className="px-4 py-2 border rounded-lg"
+            onClick={() => updateParams({ page: Math.max(currentPage - 1, 1) })}
             disabled={currentPage === 1}
+            className={`px-4 py-2 border rounded-lg ${
+              currentPage === 1
+                ? "opacity-50 cursor-not-allowed"
+                : "hover:bg-gray-100"
+            }`}
           >
             Prev
           </button>
-
           {[...Array(totalPages)].map((_, index) => {
             const page = index + 1;
 
@@ -164,7 +159,6 @@ function Products() {
               </button>
             );
           })}
-
           {/* Next */}
           <button
             onClick={() =>
@@ -172,7 +166,11 @@ function Products() {
                 page: Math.min(currentPage + 1, totalPages),
               })
             }
-            className="px-4 py-2 border rounded-lg"
+             className={`px-4 py-2 border rounded-lg ${
+              currentPage === totalPages
+                ? "opacity-50 cursor-not-allowed"
+                : "hover:bg-gray-100"
+            }`}
             disabled={currentPage === totalPages}
           >
             Next
